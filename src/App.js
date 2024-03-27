4abb7dafb44e667a924c72ba0b482a82446a8b59
Milan Matejic <milanmatejicdev@gmail.com>
2024-03-27 13:49:14 +0100

    Fixed layout of the app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import './main.scss';
-import { Col, Container,Row } from 'react-bootstrap';
-import HeroSpot from './components/Hero';
-import TutorialSection from './components/Tutorial';
-import Footer from './components/Footer';
-import NextSteps from './components/NextSteps';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./main.scss";
+import { Col, Container, Row } from "react-bootstrap";
+import HeroSpot from "./components/Hero";
+import TutorialSection from "./components/Tutorial";
+import Footer from "./components/Footer";
+import NextSteps from "./components/NextSteps";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
-
 function App() {
-  return (
-    <Router>
-      <HeroSpot />
-  <Container className="gx-0">
-    <Row>
-    
-
-    <Col className='mx-auto' xs={12} md={8}>
-    <TutorialSection />
-
-    </Col>
-    </Row>
-  </Container>
-  </Router>
-  );
+    return (
+        <Router>
+            <HeroSpot />
+            <Container>
+                <Row>
+                    <Col className="mx-auto" xs={12} md={8}>
+                        <TutorialSection />
+                    </Col>
+                </Row>
+            </Container>
+        </Router>
+    );
 }
 
 export default App;
